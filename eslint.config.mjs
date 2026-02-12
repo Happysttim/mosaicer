2a3d68c7c525a042c8198eb017be13ff40eb30b8
Happysttim <gmlsdyd1023@naver.com>
2026-02-13 01:25:05 +0900

    init: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,42 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import prettierPlugin from "eslint-plugin-prettier";
+
+export default defineConfig([
+	globalIgnores(['dist', 'vite.config.ts']),
+	{
+		files: ['**/*.{ts,tsx}'],
+		plugins: {
+			prettier: prettierPlugin,
+		},
+		extends: [
+			js.configs.recommended,
+			tseslint.configs.recommended,
+			reactHooks.configs.flat.recommended,
+			reactRefresh.configs.vite,
+		],
+		languageOptions: {
+			ecmaVersion: 2020,
+			globals: globals.browser,
+			parserOptions: {
+				project: './tsconfig.app.json',
+				tsconfigRootDir: import.meta.dirname,
+			}
+		},
+		rules: {
+      'prettier/prettier': ['error', {
+        endOfLine: 'auto',
+        singleQuote: true,
+        trailingComma: 'all'
+      }],
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+    },
+	}
+]);
