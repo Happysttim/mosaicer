32dee3c2a6191e47a0bb513b9f3e2626e3d322c0
Happysttim <gmlsdyd1023@naver.com>
2026-02-14 21:53:14 +0900

    chore: add svg assets

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,3 +3,10 @@ declare module '*.svg?react' {
   const content: FunctionComponent<SVGProps<SVGSVGElement>>;
   export default content;
 }
+
+declare module '*.svg' {
+  import React from 'react';
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
