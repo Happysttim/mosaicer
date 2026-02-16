5423a10a08c279b61dda9905724d9ffeca77ac27
Happysttim <gmlsdyd1023@naver.com>
2026-02-16 17:53:06 +0900

    feat: add browser route loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { URL } from './constants/url';
+import { CONST_URL } from './constants/url';
 import Home from './pages/home/Home';
 import Make from './pages/make/Make';
+import Result from './pages/result/Result';
+import useImageStore from './stores/image';
+
+const reset = () => {
+  const { reset } = useImageStore.getState();
+  reset();
+};
 
 const router = createBrowserRouter([
   {
-    path: URL.HOME,
+    path: CONST_URL.HOME,
     element: <Home />,
+    loader: reset,
   },
   {
-    path: URL.MAKE,
+    path: CONST_URL.MAKE,
     element: <Make />,
+    loader: reset,
+  },
+  {
+    path: CONST_URL.RESULT,
+    element: <Result />,
   },
 ]);
 
