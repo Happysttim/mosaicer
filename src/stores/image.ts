c6a100e27d01251cdfd04d7c20be3ed9400bac87
Happysttim <gmlsdyd1023@naver.com>
2026-02-19 20:48:59 +0900

    feat: add image state

--- src/stores/image.ts
+++ src/stores/image.ts
@@ -1,16 +1,20 @@
+import type { TileCounter } from '@/types/state';
 import { create } from 'zustand';
 
 interface ImageState {
   main: File | null;
   tiles: File[];
-  urls: string[];
+  urls: Map<string, string>;
 
   batchType: 'histogram' | 'random';
   opacity: number;
   scale: number;
   maxCount: number;
 
-  setUrls: (urls: string[]) => void;
+  tileCounter: Map<string, TileCounter>;
+
+  setUrl: (fileNmae: string, url: string) => void;
+  deleteUrl: (fileName: string) => void;
   setMain: (file: File) => void;
   deleteMain: () => void;
   appendTile: (...tiles: File[]) => void;
@@ -19,6 +23,10 @@ interface ImageState {
   setOpacity: (opaticy: number) => void;
   setScale: (scale: number) => void;
   setMaxCount: (maxCount: number) => void;
+  setTileCount: (fileName: string, tileCount: TileCounter) => void;
+  incrementCount: (fileName: string) => void;
+
+  hasCount: (fileName: string) => boolean;
 
   reset: () => void;
 }
@@ -26,14 +34,25 @@ interface ImageState {
 const useImageStore = create<ImageState>((set, get) => ({
   main: null,
   tiles: [],
-  urls: [],
+  urls: new Map(),
 
   batchType: 'histogram',
   opacity: 0.5,
   scale: 1.0,
   maxCount: 5000,
 
-  setUrls: (urls: string[]) => set({ urls }),
+  tileCounter: new Map(),
+
+  setUrl: (fileName: string, url: string) => {
+    const urls = new Map(get().urls);
+    set({ urls: urls.set(fileName, url) });
+  },
+  deleteUrl: (fileName: string) => {
+    const urls = new Map(get().urls);
+    urls.delete(fileName);
+
+    set({ urls });
+  },
   setMain: (file: File) => set({ main: file }),
   deleteMain: () => set({ main: null }),
   appendTile: (...tiles: File[]) => set({ tiles: [...get().tiles, ...tiles] }),
@@ -42,16 +61,39 @@ const useImageStore = create<ImageState>((set, get) => ({
   setOpacity: (opacity: number) => set({ opacity }),
   setScale: (scale: number) => set({ scale }),
   setMaxCount: (maxCount: number) => set({ maxCount }),
+  setTileCount: (fileName: string, tileCount: TileCounter) => {
+    const tileCounter = new Map(get().tileCounter);
+    set({
+      tileCounter: tileCounter.set(fileName, tileCount),
+    });
+  },
+  incrementCount: (fileName: string) => {
+    const tileCounter = new Map(get().tileCounter);
+    const tileCount = tileCounter.get(fileName);
+    if (!tileCount) {
+      return;
+    }
+
+    set({
+      tileCounter: tileCounter.set(fileName, {
+        count: tileCount.count + 1,
+        url: tileCount.url,
+      }),
+    });
+  },
+
+  hasCount: (fileName: string) => get().tileCounter.get(fileName) !== undefined,
 
   reset: () =>
     set({
       main: null,
       tiles: [],
-      urls: [],
+      urls: new Map(),
       batchType: 'histogram',
       opacity: 0.5,
       scale: 1,
       maxCount: 5000,
+      tileCounter: new Map(),
     }),
 }));
 
